b5844a328449035eabdd3ed8ea27a54dbe841c94
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2020-05-30 21:25:32 +0200

    handle infinite loop

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from mirrnum.calc import calc
+from mirrnum.calc import calc, InfiniteLoopError
 
 import matplotlib.pyplot as plt
 
@@ -9,8 +9,13 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Please also enter a number.')
         quit()
-     
-    result = calc(int(sys.argv[1]))
+    
+    try:
+        result = calc(int(sys.argv[1]))
+    except InfiniteLoopError:
+        print('Infinite loop, try another number')
+        quit()
+
     print('Result: {}'.format(result))
 
     filename = 'plot.png'
--- mirrnum/calc.py
+++ mirrnum/calc.py
@@ -1,9 +1,17 @@
-import sys
-
-sys.setrecursionlimit(10**6)
+class InfiniteLoopError(Exception):
+    pass
 
 
 mirror = lambda x: int(str(x)[::-1])
 calcnext = lambda x: abs(x - mirror(x))
 append = lambda x, y: [x.append(y), y][1]
-calc = lambda x, s=[]: [calc(append(s, calcnext(x)), s), s][1] if x > 0 else 0
+
+
+def calc(x, s=[]):
+    while x > 0:
+        if s.count(x) > 2:
+            raise InfiniteLoopError
+
+        x = append(s, calcnext(x))
+
+    return s
